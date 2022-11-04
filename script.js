1c918e03b118946e602765112e55f0398d805d5d
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-04 00:07:13 -0500

    set up page and made notes on what to code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.prompt("Please choose: rock, paper or scissors")
+// Steps
+// 1. Create randomizer for computer to pick between rps
+// 2. Make a compare function to determine user choice vs computer choice and have it return the winner based on outcome
+// 3. Select winner and add to score
+// 4. Have a user and computer score and have the score update after each turn 
+
+
+
+var question = window.prompt("Do you want to play: rock, paper, scissors?", "Let's play!");
+
+var userChoice = window.prompt("Please choose: rock, paper or scissors");
+
+let choices = ["rock", "paper", "scissors"];
