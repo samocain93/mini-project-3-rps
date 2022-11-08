f7dfc66066790d38eb8e3d21d0afb5f0264b2836
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-07 23:18:49 -0600

    added some js code

--- script.js
+++ script.js
@@ -16,9 +16,44 @@
 
 // console.log(x)
 
+var userChoice = ["rock", "paper", "scissors"]
+
+var computerChoice = ["rock", "paper" "scissors"]
+
+// Need a variable to hold scores and add to each round
+// 
+// var userScore = 0;
+// var computerScore = 0;
+// var ties = 0
+
+
+function playGame() {
+    var userPlay = prompt("Do you want to play rock, paper, scissors?");
+    var userPick = prompt("Please choose rock, paper or scissors")
+
+    var userScore = 0;
+    var computerScore = 0;
+    var ties = 0;
+
+    // Write loop to play if userPlay = true, then execute
+
+    if userPlay
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function getRandomChoice() {
     let computerChoices = ["rock", "paper", "scissors"];
     return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
-
-// console.log(getRandomChoice())
