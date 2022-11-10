3540e16fd01b338ddb33fc8c9c964a66327d9be7
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-10 11:42:06 -0600

    setting up functions and vars

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 // 1. Create randomizer for computer to pick between rps -- done!
 // 2. Make a compare function to determine user choice vs computer choice and have it return the winner based on outcome
 // 3. Select winner and add to score
-// 4. Have a user and computer score and have the score update after each turn 
-
-
+// 4. Have a user and computer score and have the score update after each turn
 
 // var question = window.prompt("Do you want to play: rock, paper, scissors?", "Let's play!");
 
@@ -16,44 +14,24 @@
 
 // console.log(x)
 
-var userChoice = ["rock", "paper", "scissors"]
 
-var computerChoice = ["rock", "paper" "scissors"]
 
-// Need a variable to hold scores and add to each round
-// 
-// var userScore = 0;
-// var computerScore = 0;
-// var ties = 0
+
+
 
 
 function playGame() {
-    var userPlay = prompt("Do you want to play rock, paper, scissors?");
-    var userPick = prompt("Please choose rock, paper or scissors")
 
-    var userScore = 0;
-    var computerScore = 0;
-    var ties = 0;
-
-    // Write loop to play if userPlay = true, then execute
-
-    if userPlay
 }
 
+function playRound(playerSelection, computerSelection) {
 
+}
 
+function playerChoice() {
 
+}
 
+function computerChoice() {
 
-
-
-
-
-
-
-
-
-function getRandomChoice() {
-    let computerChoices = ["rock", "paper", "scissors"];
-    return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
